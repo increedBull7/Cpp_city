c73f9160d96488660e7cf29c0f1dcb867320825e
Atul Raat <atulraat@Atuls-MacBook-Air.local>
2021-03-14 04:26:10 +0530

    supporting files

---
+++ pointers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int *q;
+    int a = 10;
+    q= &a;
+    cout<<q<<endl;
+    cout<<*q<<endl;
+    int arr[10];
+    cout<<arr<<endl;
+
+
+}
